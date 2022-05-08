92f9e84e01e1ddba152b1b229578fd12e991421c
moaz saeed <moaz.saeed@intigral.net>
2022-05-09 01:06:54 +0400

    Initial Commit

---
+++ MzPracticeProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MzPracticeProject
+//
+//  Created by Moaz Saeed (c) on 09/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
